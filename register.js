3a6eb0248ced142d124389c2c44a64c343864fdb
Paul Baumgarten <pbaumgarten@gmail.com>
2021-02-05 11:06:15 +0800

    initial

---
+++ register.js
@@ -0,0 +1,16 @@
+const SERVICE_WORKER = '/service-worker.js';
+function registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        console.log("Registering service-worker.js");
+        navigator.serviceWorker.register(SERVICE_WORKER).then(function(registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+    } else {
+        console.log("Browser is not compatible with service workers.")
+    }
+}
+
+window.onload = registerServiceWorker;
