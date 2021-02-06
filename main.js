cbf6e5c7e97fd6a22b7501d5145cfbb7d19617b7
Paul Baumgarten <pbaumgarten@gmail.com>
2021-02-06 16:23:39 +0800

    post video version

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let BACKGROUND, BASE, BOTTOM_PIPE, TOP_PIPE, BIRD, DIE, HIT, WING, SWOOSH, FONT;
 let game_size = [288, 512];
-let bird = [50, 250];
-let pipe = [300, 100];
-let bird_frame_number = 0;
-let jump = 0;
-let lives = 5;
+let game_running = false;
+let pipe = [500, 100];
 let pipe_gap = 100;
 let pipe_speed = 10;
-let game_running = false;
+let bird = [50, 250];
+let jump = 0;
+let lives = 5;
+let points = 0;
 
-function preload() {
-    // Load media files
+function preload() { // Load all the media files
     BACKGROUND = loadImage('media/background.png');
-    BASE = loadImage("media/base.png");
+    BASE = loadImage('media/base.png');
     TOP_PIPE = loadImage("media/pipe-top.png");
     BOTTOM_PIPE = loadImage("media/pipe-bottom.png");
     BIRD = loadImage("media/flappybird-animation.gif");
@@ -24,51 +23,49 @@ function preload() {
 }
 
 function setup() {
-    game_size = [windowWidth, windowHeight]; // windowWidth & windowHeight are auto created by p5.js
+    game_size = [windowWidth, windowHeight];
     createCanvas(game_size[0], game_size[1]);
-    BACKGROUND.resize(game_size[0], game_size[1]); // Resize to match screen
+    BACKGROUND.resize(game_size[0], game_size[1]);
     BASE.resize(game_size[0], BASE.height);
     frameRate(25);
 }
 
 function is_collision(im1,x1,y1, im2,x2,y2) {
     if ((x1+im1.width < x2) || (x1 > x2+im2.width) || (y1+im1.height < y2) || (y1 > y2+im2.height)) {
-        return false;
+        return false; // there is no collision
     } else {
-        return true;
+        return true; // there is a collision
     }
 }
-  
-function play() { // One frame of the game (1/25th of a second)
+
+function play() { // 1/25th of a second
     // Pipes
     pipe[0] -= pipe_speed;
-    if (pipe[0] < -BOTTOM_PIPE.width) {
+    if (pipe[0] < -TOP_PIPE.width) {
         pipe[0] = game_size[0];
-        pipe[1] = 50 + random(game_size[1]-200);
+        pipe[1] = 50 + random(game_size[1] - 200);
         pipe_gap = 100 + random(100);
+        points += 1;
     }
     image(TOP_PIPE, pipe[0], pipe[1]-TOP_PIPE.height);
     image(BOTTOM_PIPE, pipe[0], pipe[1]+pipe_gap);
     // Bird movement
-    if (mouseIsPressed && ! WING.isPlaying()) {
+    if (mouseIsPressed && ! WING.isPlaying() ) {
         jump = 20;
         WING.play();
     }
-    bird[1] -= jump;
     if (jump > -20) {
         jump -= 2;
     }
-    let bird_frame = BIRD.setFrame(bird_frame_number); // Create a graphics object 34x24 pixels
+    bird[1] -= jump;
     image(BIRD, bird[0], bird[1]);
-    bird_frame_number = (bird_frame_number+1) % 3;
     // Detect collision
-    if (is_collision(BIRD, bird[0], bird[1], TOP_PIPE, pipe[0], pipe[1]-TOP_PIPE.height) ||
+    if (is_collision(BIRD, bird[0], bird[1], TOP_PIPE, pipe[0], pipe[1]-TOP_PIPE.height) || 
         is_collision(BIRD, bird[0], bird[1], BOTTOM_PIPE, pipe[0], pipe[1]+pipe_gap)) {
-        lives -= 1;
-        bird = [50, 250];
-        pipe[0] = game_size[0] + pipe_speed*25;
-        pipe[1] = 50 + random(game_size[1]-200);
+        pipe[0] = game_size[0];
+        pipe[1] = 50 + random(game_size[1] - 200);
         pipe_gap = 100 + random(100);
+        lives -= 1;
         if (lives < 0) {
             game_running = false;
             DIE.play();
@@ -76,24 +73,26 @@ function play() { // One frame of the game (1/25th of a second)
             HIT.play();
         }
     }
-    // Print lives remaining
+    // Print lives and scores
     text(lives, 10, 20);
+    text(points, game_size[0]-20, 20);
 }
 
 function draw() {
-    // Paint background
-    image(BACKGROUND, 0, 0); // for cropping https://p5js.org/reference/#/p5/get
+    image(BACKGROUND, 0,0);
     image(BASE, 0, game_size[1]-BASE.height);
-    if (game_running) { // Game is happening!
-        play();
-    } else {
-        textAlign(CENTER, CENTER); // horizontal, vertical
+    if (! game_running) {
+        textAlign(CENTER, CENTER);
         textSize(40);
         fill(255);
         text('Flappy bird', game_size[0]/2, game_size[1]/3);
         text('Click to start', game_size[0]/2, game_size[1]*5/6);
         if (mouseIsPressed) {
             game_running = true;
+            lives = 5;
+            points = 0;
         }
+    } else {
+        play();
     }
 }
